716b7e682b160ce4ced438b8b6acfb4f80c4c7e3
Alex Conway <alex@ac-media.co.uk>
2015-06-29 15:11:37 +0100

    Adds express and stuff

---
+++ server.js
@@ -0,0 +1,20 @@
+/**
+ * Created by alexconway on 29/06/15.
+ */
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+app.use(express.static(__dirname + '/app'));                 // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// listen (start app with node server.js) ======================================
+app.listen(80);
+console.log("App listening on port 80");
